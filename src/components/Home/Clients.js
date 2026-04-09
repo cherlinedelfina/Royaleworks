af8dfd53317c384ca45a37c05c36cc4195ffb0a8
Cherline Delfina Tandra <cher@Cherlines-MacBook-Pro.local>
2026-04-09 18:41:04 +1000

    final

--- src/components/Home/Clients.js
+++ src/components/Home/Clients.js
@@ -1,29 +1,51 @@
 import React from "react";
 import "../../css/Clients.css";
 
-import kws from "../../images//partner/partners_logo1.png";
-import geps from "../../images//partner/partners_logo2.png";
-import protergia from "../../images/partner/partners_logo3.jpg";
+import elm from "../../images/partner/partners_logo1.png";
+import edelweiss from "../../images/partner/partners_logo2.png";
+import nurse from "../../images/partner/partners_logo3.jpg";
 
 const Clients = () => {
+  const partners = [
+    { 
+      img: elm, 
+      name: "Elm Embrace",
+      desc: "Premium home care solutions for Melbourne families."
+    },
+    { 
+      img: edelweiss, 
+      name: "Edelweiss Care",
+      desc: "Specialized in elderly care and wellness programs."
+    },
+    { 
+      img: nurse, 
+      name: "Nurse N Care",
+      desc: "Professional nursing and personal care services."
+    },
+  ];
+
+
   return (
     <section className="clients-section" id="partners">
       <div className="clients-container">
+
+        {/* TOP */}
         <h2 className="clients-title">PARTNERS</h2>
+        <p className="clients-desc">
+          Partnering with clients who expect consistent, high-standard results.
+        </p>
 
-        <div className="clients-logos">
-          <div className="client-logo">
-            <img src={kws} alt="KWS" />
-          </div>
-
-          <div className="client-logo">
-            <img src={protergia} alt="Protergia" />
-          </div>
-
-          <div className="client-logo">
-            <img src={geps} alt="GEPS" />
-          </div>
+        {/* CARDS */}
+        <div className="clients-cards">
+          {partners.map((partner, index) => (
+            <div className="client-card" key={index}>
+              <img src={partner.img} alt={partner.name} />
+              <h4>{partner.name}</h4>
+              <p className="client-desc">{partner.desc}</p>
+            </div>
+          ))}
         </div>
+
       </div>
     </section>
   );
